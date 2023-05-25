104188d4f7b186f2ba29655ec4c7ad9168af8f39
Ahmad Azwan bin Muhammad Zuharimi <ahmad.azwan@simedarbyproperty.com>
2023-05-25 14:58:01 +0800

    updated files

---
+++ carigold-scraper/testrun.py
@@ -0,0 +1,19 @@
+import boto3
+from io import StringIO
+import scrapy
+import subprocess
+
+subprocess.run("""scrapy runspider carigold_spider.py -o carigold.csv""")
+
+file_name = 'carigold.csv'
+# print(file_name)
+
+csv_buffer = StringIO()
+
+s3 = boto3.resource('s3',
+                    aws_access_key_id='AKIAY3JUX37XAKZOZZFT',
+                    aws_secret_access_key='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')
+
+# object = s3.Object('01dev', f'carigold/{file_name}').put(Body=file_name)
+
+s3.meta.client.upload_file('carigold.csv', '01dev', 'carigold/carigold.csv')
